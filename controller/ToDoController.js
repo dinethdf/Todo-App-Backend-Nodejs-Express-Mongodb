a71dbc576cf899fa15ea8c4fe7dc67a5c28fb234
dineth Dilshan <E2145112@bit.uom.lk>
2023-08-29 21:57:03 +0530

    Back End Develop 1 day
    Module create contolers route paths ande requenst create adding

---
+++ controller/ToDoController.js
@@ -0,0 +1,18 @@
+const ToDoModel = require("../models/TodoModel");
+
+module.exports.getToDo = async (req, res) => {
+  const todo = await ToDoModel.find();
+  res.send(todo);
+};
+
+module.exports.saveToDo = (req, res) => {
+  const { text } = req.body;
+
+  ToDoModel.create({ text })
+    .then((data) => {
+      console.log("Added Successfully...");
+      console.log(data);
+      res.send(data);
+    })
+    .catch((err) => console.log(err));
+};
