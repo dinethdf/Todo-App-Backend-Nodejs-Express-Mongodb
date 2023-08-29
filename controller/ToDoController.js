eac56e43127248edc34d1a858e4d4c67aab46d27
dineth Dilshan <E2145112@bit.uom.lk>
2023-08-29 23:15:52 +0530

    Back end CRUD Function Done
    All create get update delete function done in back end

--- controller/ToDoController.js
+++ controller/ToDoController.js
@@ -10,9 +10,27 @@ module.exports.saveToDo = (req, res) => {
 
   ToDoModel.create({ text })
     .then((data) => {
+      res.send(data);
       console.log("Added Successfully...");
       console.log(data);
-      res.send(data);
     })
+    .catch((err) => console.log(err));
+};
+
+module.exports.updateToDo = (req, res) => {
+  const { _id, text } = req.body;
+
+  ToDoModel.findByIdAndUpdate(_id, { text })
+    .then(() => res.set(201).send("Updated Successfully..."))
+    .catch((err) => console.log(err));
+};
+
+module.exports.deleteToDo = (req, res) => {
+  const { _id } = req.body;
+
+  console.log("id ---> ", _id);
+
+  ToDoModel.findByIdAndDelete(_id)
+    .then(() => res.set(201).send("Deleted Successfully..."))
     .catch((err) => console.log(err));
 };
--- routes/ToDoRoute.js
+++ routes/ToDoRoute.js
@@ -1,9 +1,16 @@
 const { Router } = require("express");
-const { getToDo, saveToDo } = require("../controller/ToDoController");
+const {
+  getToDo,
+  saveToDo,
+  updateToDo,
+  deleteToDo,
+} = require("../controller/ToDoController");
 
 const router = Router();
 
 router.get("/", getToDo);
 router.post("/save", saveToDo);
+router.post("/update", updateToDo);
+router.post("/delete", deleteToDo);
 
 module.exports = router;
